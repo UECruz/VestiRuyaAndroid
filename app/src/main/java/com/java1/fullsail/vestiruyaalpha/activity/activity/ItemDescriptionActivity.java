c6d3b0cafacd615c14312d1574aa35ba3b30565c
Ursula Cruz <AlexSukran@gmail.com>
2019-03-04 23:54:56 -0500

    Commit message

--- app/src/main/java/com/java1/fullsail/vestiruyaalpha/activity/activity/ItemDescriptionActivity.java
+++ app/src/main/java/com/java1/fullsail/vestiruyaalpha/activity/activity/ItemDescriptionActivity.java
@@ -1,5 +1,6 @@
 package com.java1.fullsail.vestiruyaalpha.activity.activity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
